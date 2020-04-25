9844f1ecdf6257d244cf651fdcce399a658b9d8b
Edward Jacobs <eddielawrence23@gmail.com>
2020-04-25 01:14:57 -0500

    Add associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+Channel.create(name: 'general')
+Channel.create(name: 'paris')
+Channel.create(name: 'react')
+
+User.create(email: 'eddielawrence23@gmail.com')
+
+Message.create(user: 'eddielawrence23@gmail.com' channel: 'general' content: "Hello world");
