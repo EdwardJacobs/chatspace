f0af29e64418f13d2d304d98e8a7b19a58864bb3
Edward Jacobs <eddielawrence23@gmail.com>
2020-04-25 19:58:10 -0500

    FEAT: changing channel

--- app/javascript/chat/components/message.jsx
+++ app/javascript/chat/components/message.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { emojify } from 'react-emojione';
 
 function strToRGB(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i += 1) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  if (str && str.length > 0) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i += 1) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    const c = (hash & 0x00FFFFFF)
+      .toString(16)
+      .toUpperCase();
+    return `#${"00000".substring(0, 6 - c.length)}${c}`;
   }
-  const c = (hash & 0x00FFFFFF)
-    .toString(16)
-    .toUpperCase();
-  return `#${"00000".substring(0, 6 - c.length)}${c}`;
 }
 
 const Message = (props) => {
