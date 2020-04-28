2356bda525b1498d75cd100ce0e646d294248e96
Edward Jacobs <eddielawrence23@gmail.com>
2020-04-28 17:37:48 -0500

    test channels controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,10 +1,8 @@
 class ChannelsController < ApplicationController
   def show
-    if params[:id].blank?
-      redirect_to channel_path(Channel.first.name)
-    else
+
       @channel = Channel.find_by(name: params[:id])
       @channels = Channel.all
-    end
+
   end
 end
