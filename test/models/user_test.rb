89ae1af8c380dee1072d7f747ccae2e5aab65472
Edward Jacobs <eddielawrence23@gmail.com>
2020-05-20 18:55:31 -0500

    Add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,17 +2,21 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(email: 'ed@gmail.com')
+    @user = User.new(email: 'ed@gmail.com', password: 'testtest')
   end
   test 'valid user' do
     assert @user.valid?
   end
 
-  test 'invalid without name' do
-
+  test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?, 'saved user without a name'
+    assert_not_nil @user.errors[:email], 'no validation error for email present'
   end
 
-  test 'invalid without email' do
-
+  test 'invalid without password' do
+    @user.password = nil
+    refute @user.valid? 'saved user without a password'
+    assert_not_nil @user.errors[:password], 'no validation error for password present'
   end
 end
